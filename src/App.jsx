05362ea9a74d416b11266bff0587c1996f0ca948
Nathan Smith <npatricksmith@gmail.com>
2026-01-29 11:50:05 -0500

    UI Polish: Add edit icon to vehicle name input

--- src/App.jsx
+++ src/App.jsx
@@ -91,6 +91,10 @@ function App() {
                 onChange={(e) => handleInputChange(index, 'name', e.target.value)}
                 placeholder="Vehicle Name"
               />
+              <svg className="edit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+              </svg>
             </div>
 
             <div className="card results-card-mini">
